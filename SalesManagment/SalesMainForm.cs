8557f251e14d865abc9536a7146dc1a64199f495
User <User@DESKTOP-QLL01CM>
2018-09-25 13:31:31 +0200

    me edit

--- SalesManagment/SalesMainForm.cs
+++ SalesManagment/SalesMainForm.cs
@@ -217,7 +217,7 @@ namespace SalesManagment
             xtraTabPage.Controls.Clear();
 
             xtraTabControlSalesContent.SelectedTabPage = xtraTabPage;
-            RecordSellPriceForm objForm = new RecordSellPriceForm(productsSellPriceForm,xtraTabControlSalesContent);
+            SetSellPrice objForm = new SetSellPrice(productsSellPriceForm,xtraTabControlSalesContent);
             objForm.TopLevel = false;
 
             xtraTabPage.Controls.Add(objForm);
